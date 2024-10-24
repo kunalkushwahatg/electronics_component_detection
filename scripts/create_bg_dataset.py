321ee6e2c40e5e34a53b8ce196a8ec30371b5c19
kunalkushwahatg <kunalkushwahatg@gmail.com>
2024-10-24 21:48:46 +0530

    fixed minor bugs added argparser for use of kaggle

--- scripts/create_bg_dataset.py
+++ scripts/create_bg_dataset.py
@@ -1,8 +1,23 @@
 from video_to_image import video_to_images
 import os
 import tqdm
+import argparse
 
-video_path = 'data/'
-output_path = 'background/'
+# Set up argument parser
+parser = argparse.ArgumentParser(description="Extract frames from videos")
+
+parser.add_argument('--video-path', type=str, help="Path to the directory containing videos" , default='./videos')
+parser.add_argument('--output-path', type=str, help="Path to the output directory" , default='./background')
+
+video_path = parser.parse_args().video_path
+output_path = parser.parse_args().output_path
+
+
+
+
 for video in tqdm.tqdm(os.listdir(video_path)):
     video_to_images(os.path.join(video_path, video), output_path, fps=5)
+
+total_images = len(os.listdir(output_path))
+print(f"Extracted {total_images} images from {len(os.listdir(video_path))} videos to {output_path}")
+
--- scripts/create_dataset.py
+++ scripts/create_dataset.py
@@ -15,33 +15,31 @@ yaml_path = './'
 base_path = './datasets'
 
 # #setup the yolo folder structure
-# setup_yolo_folder_structure(output_path)
+setup_yolo_folder_structure(output_path)
 
 
+# Get the list of component image paths
+component_paths = list(Path(components_folder).glob('**/*.jpg'))
 
+# Get the list of class names and create a mapping
+class_names = os.listdir(components_folder)
+class_names = {class_name: i for i, class_name in enumerate(class_names)}
 
-# # Get the list of component image paths
-# component_paths = list(Path(components_folder).glob('**/*.jpg'))
+# Create background image paths
+bg_paths = AugmentationGenerator.get_background_image_paths(bg_folder)
 
-# # Get the list of class names and create a mapping
-# class_names = os.listdir(components_folder)
-# class_names = {class_name: i for i, class_name in enumerate(class_names)}
+# Create an instance of the AugmentationGenerator class
+generator = AugmentationGenerator(component_paths, bg_paths, output_path, class_names)
 
-# # Create background image paths
-# bg_paths = AugmentationGenerator.get_background_image_paths(bg_folder)
-
-# # Create an instance of the AugmentationGenerator class
-# generator = AugmentationGenerator(component_paths, bg_paths, output_path, class_names)
-
-# # Generate the augmented images and annotations
-# generator.create_augmented_images_with_annotations(num_images=1000)
+# Generate the augmented images and annotations
+generator.create_augmented_images_with_annotations(num_images=1000)
 
 
 #move the images and labels to the val folder
 move_files_to_val(base_path, val_split=0.2)
 
 #generate yaml file
-# generate_yaml_file(yaml_path, len(class_names), class_names)
+generate_yaml_file(yaml_path, len(class_names), class_names)
 
 
 
