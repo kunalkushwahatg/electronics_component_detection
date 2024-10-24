1d57ec8633843f464492d6cb82c7b3b3c23a1062
kunalkushwahatg <kunalkushwahatg@gmail.com>
2024-10-24 21:57:30 +0530

    error handle

--- scripts/create_bg_dataset.py
+++ scripts/create_bg_dataset.py
@@ -8,6 +8,7 @@ parser = argparse.ArgumentParser(description="Extract frames from videos")
 
 parser.add_argument('--video-path', type=str, help="Path to the directory containing videos" , default='./videos')
 parser.add_argument('--output-path', type=str, help="Path to the output directory" , default='./background')
+parser.add_argument('--fps', type=int, help="Frames per second to extract" , default=5)
 
 video_path = parser.parse_args().video_path
 output_path = parser.parse_args().output_path
@@ -21,3 +22,4 @@ for video in tqdm.tqdm(os.listdir(video_path)):
 total_images = len(os.listdir(output_path))
 print(f"Extracted {total_images} images from {len(os.listdir(video_path))} videos to {output_path}")
 
+#python create_bg_dataset.py                    
